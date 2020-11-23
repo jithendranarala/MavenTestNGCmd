ce7febd366154e0f3004ee604f31e2879ed7432b
Jithendra <jithendra.narala@gmail.com>
2020-11-22 19:36:28 -0500

    Maven First

---
+++ src/test/java/com/test1.java
@@ -0,0 +1,31 @@
+package com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void test() {
+		System.out.println("Hai Jithendra");
+	}
+	
+	@Test
+	public void launchBr() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Apr13_2019\\Resources\\chromedriver.exe");
+		 
+		// Initialize browser
+		WebDriver driver=new ChromeDriver();
+		 
+		// Open facebook
+		driver.get("http://www.facebook.com");
+		 
+		// Maximize browser
+		 
+		driver.manage().window().maximize();
+		
+		driver.close();
+	}
+
+}
